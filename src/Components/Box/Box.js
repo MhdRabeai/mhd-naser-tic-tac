8d049cacef09613f298ab9434e1f8298a157ee85
MhdRabeai <mhd.rabea.naser@gmail.com>
2024-09-29 10:44:57 +0300

    update

--- src/Components/Box/Box.js
+++ src/Components/Box/Box.js
@@ -5,7 +5,7 @@ export default function Box(props) {
     <div
       onClick={props.handleClickDiv}
       id={props.id}
-      className="game-screen__cell flex items-center justify-center w-20 mb-4 h-20 bg-transparent border-white border-2 rounded-xl bg-white bg-opacity-0 hover:bg-opacity-25 duration-300"
+      className="game-screen__cell flex items-center justify-center w-20 mb-4 h-20 bg-transparent border-white border-2 rounded-xl bg-white bg-opacity-0 hover:bg-opacity-25 duration-0"
     ></div>
   );
 }
